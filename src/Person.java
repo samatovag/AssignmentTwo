5aac9e7a60547248d7b1c2e5766103dedbe14b9b
Samatovag <230026@astanait.edu.kz>
2024-01-25 23:39:42 +0600

    yjxykxyvy

--- src/Person.java
+++ src/Person.java
@@ -1,3 +1,43 @@
-public class Person {
-    private
+interface Payable {
+    double getPaymentAmount();
+}
+public class Person implements Comparable<Person>, Payable {
+
+    private int id;
+    private String name;
+    private String surname;
+
+    private static int nextId = 1;
+
+    // constructors
+    public Person() {
+        this.id = nextId++;
+    }
+
+    // toString
+    @Override
+    public String toString() {
+
+    }
+
+    // Getters and setters
+    public int getId() {
+        return id;
+    }
+    public String getName() {
+        return name;
+    }
+
+
+    // Implementation of Payable interface method
+    @Override
+    public double getPaymentAmount() {
+        return 0.00;
+    }
+
+    @Override
+    public int compareTo(Person other) {
+        return Double.compare(this.getPaymentAmount(), other.getPaymentAmount());
+    }
+
 }
