00cca55d103c6550ee07bc2b092f486dc7b85e32
Samatovag <230026@astanait.edu.kz>
2024-01-27 14:41:32 +0600

    yjxykxyvy

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,10 @@
 import java.util.ArrayList;
-import java.util.
+import java.util.Collections;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
+
+// printData method
